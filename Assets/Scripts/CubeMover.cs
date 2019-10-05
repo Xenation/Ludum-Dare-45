ecd1cabfc18f1239ee9a08476a58835de1461b0b
Xenation <xenations.111@gmail.com>
2019-10-05 03:59:31 +0200

    Prototyping Controller

---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace LD45 {
+	public class CubeMover : MonoBehaviour {
+
+		public float walkForce = 2f;
+		public float rotateForce = 10f;
+		public float jumpForce = 10f;
+
+		private Rigidbody rb;
+
+		private void Start() {
+			rb = GetComponent<Rigidbody>();
+		}
+
+		private void Update() {
+			Ray groundRay = new Ray(transform.position, Vector3.down);
+			RaycastHit hit;
+			if (Physics.Raycast(groundRay, out hit, .6f, LayerMask.GetMask("Ground"))) {
+				if (Input.GetKeyDown(KeyCode.Space)) {
+					rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+				}
+			}
+
+
+			Vector3 forceVec = Vector3.zero;
+
+			if (Input.GetKey(KeyCode.Z)) {
+				forceVec += Vector3.right * walkForce;
+			}
+			if (Input.GetKey(KeyCode.S)) {
+				forceVec -= Vector3.right * walkForce;
+			}
+			if (Input.GetKey(KeyCode.Q)) {
+				forceVec += Vector3.forward * walkForce;
+			}
+			if (Input.GetKey(KeyCode.D)) {
+				forceVec -= Vector3.forward * walkForce;
+			}
+			if (Input.GetKey(KeyCode.E)) {
+				forceVec += Vector3.up * rotateForce;
+			}
+			if (Input.GetKey(KeyCode.A)) {
+				forceVec -= Vector3.up * rotateForce;
+			}
+
+			//rb.AddForce(forceVec);
+			rb.AddTorque(forceVec);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LD45 {
+	public class Follow : MonoBehaviour {
+
+		public Transform toFollow;
+		public float lerpFactor = .8f;
+
+		private void Update() {
+			if (toFollow == null) return;
+			transform.position = Vector3.Lerp(transform.position, toFollow.position, lerpFactor);
+		}
+
+	}
+}
